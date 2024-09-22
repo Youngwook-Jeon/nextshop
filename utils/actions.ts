1427a1a053181fda14bb2a58cb6a63c18850af83
Youngwook-Jeon <ela87@naver.com>
2024-09-23 02:52:39 +0900

    Add components related to form inputs

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import db from "@/utils/db";
 import { redirect } from "next/navigation";
 
@@ -35,4 +37,13 @@ export const fetchSingleProduct = async (productId: string) => {
   if (!product) redirect("/products");
 
   return product;
+};
+
+export const createProductAction = async (
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+  prevState: any,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "Product is created" };
 };
