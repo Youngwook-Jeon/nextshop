05812578519c44bedad706a3dc00211195209770
Youngwook-Jeon <ela87@naver.com>
2024-09-27 21:24:51 +0900

    Save product images to the Supabase bucket

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use server";
 
-import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+
+import db from "@/utils/db";
+import { imageSchema, productSchema, validateWithZodSchema } from "./schemas";
+import { uploadImage } from "./supabase";
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this route");
+
+  return user;
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -42,8 +61,26 @@ export const fetchSingleProduct = async (productId: string) => {
 export const createProductAction = async (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
   prevState: any,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "Product is created" };
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const file = formData.get("image") as File;
+    const validatedFields = validateWithZodSchema(productSchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file });
+    const fullPath = await uploadImage(validatedFile.image);
+
+    await db.product.create({
+      data: {
+        ...validatedFields,
+        image: fullPath,
+        clerkId: user.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/admin/products");
 };
