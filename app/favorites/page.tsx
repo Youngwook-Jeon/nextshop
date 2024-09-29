619e179a959bf4d882ef0bf1ccdf5540c78bc6ec
Youngwook-Jeon <ela87@naver.com>
2024-09-29 22:11:30 +0900

    Fetch and show favorites of a user

---
+++ app/favorites/page.tsx
@@ -0,0 +1,16 @@
+import SectionTitle from "@/components/global/SectionTitle";
+import ProductsGrid from "@/components/products/ProductsGrid";
+import { fetchUserFavorites } from "@/utils/actions";
+
+export default async function Favorites() {
+  const favorites = await fetchUserFavorites();
+  if (favorites.length === 0)
+    return <SectionTitle text="You have no favorites yet." />;
+
+  return (
+    <div>
+      <SectionTitle text="Favorites" />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  );
+}
