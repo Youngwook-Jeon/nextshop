4ad88b3cb9c237efc7803030781b973de574849f
Youngwook-Jeon <ela87@naver.com>
2024-10-07 22:06:07 +0900

    Add models related to Cart / Implement their api actions

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,10 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useAuth } from "@clerk/nextjs";
+import { useState } from "react";
+
+import { addToCartAction } from "@/utils/actions";
+import SelectProductAmount, { Mode } from "./SelectProductAmount";
+import { ProductSignInButton, SubmitButton } from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+
 export default function AddToCart({ productId }: { productId: string }) {
+  const [amount, setAmount] = useState(1);
+  const { userId } = useAuth();
+
   return (
-    <Button className="capitalize mt-8" size="lg">
-      Add to cart
-    </Button>
+    <div className="mt-4">
+      <SelectProductAmount
+        mode={Mode.SingleProduct}
+        amount={amount}
+        setAmount={setAmount}
+      />
+      {userId ? (
+        <FormContainer action={addToCartAction}>
+          <input type="hidden" name="productId" value={productId} />
+          <input type="hidden" name="amount" value={amount} />
+          <SubmitButton text="add to cart" size="default" className="mt-8" />
+        </FormContainer>
+      ) : (
+        <ProductSignInButton />
+      )}
+    </div>
   );
 }
